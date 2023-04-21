39e075eb8be519368605a888a94e0d368a11ea23
choijan78 <choijan78@gmail.com>
2023-04-21 21:12:26 +0900

    save db

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,8 @@
 import Video from "../models/Video";
 
-export const home = (req, res) => {
-  Video.find({}, (error, videos) => {
-    return res.render("home", { pageTitle: "Home", videos: [] });
-  });
+export const home = async (req, res) => {
+  const videos = await Video.find({});
+  return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
   const { id } = req.params;
@@ -22,9 +21,20 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
-  const { title } = req.body;
-  videos.push(newVideo);
-  // return console.log(videos);
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  const video = new Video({
+    title,
+    description,
+    createAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
+  // console.log(video);
+  const dbVideo = await video.save();
+  console.log(dbVideo);
   return res.redirect("/");
 };
