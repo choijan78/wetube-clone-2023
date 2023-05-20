23658e622bc108a7178837f3da4dc9173fcbd86b
choijan78 <choijan78@gmail.com>
2023-05-20 17:21:50 +0900

    #7.12 MongoStore
    세션을 몽고디비에 저장,

--- src/init.js
+++ src/init.js
@@ -3,7 +3,7 @@ import "./models/Video";
 import "./models/User";
 import app from "./server";
 
-const PORT = 4100;
+const PORT = 4000;
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
