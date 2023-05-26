e1c4be7abea33f3529c81e3e36527e4d1e7c5f8b
choijan78 <choijan78@gmail.com>
2023-05-26 20:05:33 +0900

    #11.3 video player volume

---
+++ src/client/js/videoPlayer.js
@@ -0,0 +1,46 @@
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volumeRange = document.getElementById("volume");
+
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
+const handlePlayClick = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
+};
+
+const handlePause = () => (playBtn.innerText = "Play");
+const handlePlay = () => (playBtn.innerText = "Pause");
+
+const handleMute = (e) => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "Untmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : 0.5;
+};
+
+const handleVolumeChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  volumeValue = value;
+  video.volumn = value;
+};
+
+playBtn.addEventListener("Click", handlePlayClick);
+muteBtn.addEventListener("Click", handleMute);
+volumeRange.addEventListener("input", handleVolumeChange);
