b0205ef4c6cc741072bf271c0ae483a38ee64694
choijan78 <choijan78@gmail.com>
2023-06-09 23:38:24 +0900

    comment part hard

--- src/server.js
+++ src/server.js
@@ -4,8 +4,8 @@ import session from "express-session";
 import flash from "express-flash";
 import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
-import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 import apiRouter from "./routers/apiRouter";
 import { localsMiddleware } from "./middlewares";
 
@@ -17,26 +17,16 @@ app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(
   session({
     secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: {
-      maxAge: 86400000,
-    },
     store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
-
 app.use(flash());
 app.use(localsMiddleware);
-app.use((req, res, next) => {
-  res.header("Cross-Origin-Embedder-Policy", "require-corp");
-  res.header("Cross-Origin-Opener-Policy", "same-origin");
-  next();
-});
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
