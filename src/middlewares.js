aedb5137fa06bb434ca0d6cf297246f56b2188fd
choijan78 <choijan78@gmail.com>
2023-06-10 23:34:44 +0900

    Revert "use s3"
    This reverts commit 78c0bebcc52250e00a18ad949c1c227d5c0a01ba.

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,18 +1,4 @@
 import multer from "multer";
-import multerS3 from "multer-s3";
-import aws from "aws-sdk";
-
-const s3 = new aws.S3({
-  credentials: {
-    accessKeyId: process.env.AWS_ID,
-    secretAccessKey: process.env.AWS_SECRET,
-  },
-});
-
-const multerUploader = multerS3({
-  s3: s3,
-  bucket: "wetuberolo",
-});
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
@@ -42,10 +28,8 @@ export const publicOnlyMiddleware = (req, res, next) => {
 export const avatarUpload = multer({
   dest: "uploads/avatars/",
   limits: { fileSizs: 3000000 },
-  storage: multerUploader,
 });
 export const videoUpload = multer({
   dest: "uploads/videos/",
   limits: { fileSize: 100000000 },
-  storage: multerUploader,
 });
